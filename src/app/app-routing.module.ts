f029b0d6c22a15e3be83b340147346d85310672f
axript-tech <70604172+axript-tech@users.noreply.github.com>
2023-09-07 04:12:28 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-sections/page-not-found/page-not-found.component';
 
 //const routes: Routes = [];
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: '**', pathMatch: 'full', redirectTo: '/page-not-found' },
   { path: 'dashboard', component: DashboardComponent},
+  { path: '**', component: PageNotFoundComponent },
+
   
 ];
 
